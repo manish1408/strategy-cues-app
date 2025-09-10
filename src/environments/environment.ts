ed6de16045cebf6999667ddb7bb1817223f47f14
manish1408 <manish.gautam.india@gmail.com>
2025-09-10 10:31:02 +0530

    Update environment configuration with backend API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  APIUrl: 'http://127.0.0.1:8000/api/v1/',
-  // APIUrl: 'https://api.miloassistant.ai/api/v1',
+  // APIUrl: 'http://127.0.0.1:8000/api/v1/',
+  APIUrl: 'https://strategy-cues-backend.redsky-91a84224.eastus2.azurecontainerapps.io/api/v1/',
   // APIUrlFollowUp: 'https://be.miloassistant.ai/api/v1',
   // squidexApiUrl: 'https://cms.distinctcloud.io/api/content/milo/',
   // squidexClientId: 'milo:dcl-cms',
