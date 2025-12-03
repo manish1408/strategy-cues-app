71069a6765cdf28ba27377b17da8cbf95075bc14
manish1408 <manish.gautam.india@gmail.com>
2025-12-03 20:28:11 +0530

    Updated chrome extension last update date

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   // APIUrl: 'http://127.0.0.1:8000/api/v1/',
   APIUrl: 'https://strategy-cues-backend.redsky-91a84224.eastus2.azurecontainerapps.io/api/v1/',
   chromeExtensionUrl: 'https://drive.google.com/drive/folders/1s1CoUWNNqon_n79zzBCED1uDXPSQ_VCA?usp=sharing',
-  chromeExtensionLastUpdated: '2025-11-20'
+  chromeExtensionLastUpdated: '2025-12-03'
 };
