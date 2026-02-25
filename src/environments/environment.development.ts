92d5067cb46f56d11dd1ccdb8a5d07109aa82088
manish1408 <manish.gautam.india@gmail.com>
2026-02-25 14:20:38 +0530

    Chrome extension date updates

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   // APIUrl: 'http://127.0.0.1:8000/api/v1/',
   APIUrl: 'https://strategy-cues-backend.redsky-91a84224.eastus2.azurecontainerapps.io/api/v1/',
   chromeExtensionUrl: 'https://drive.google.com/file/d/15uh4U36soMyRUsQSCzE992bI1sxKpWgf/view?usp=sharing',
-  chromeExtensionLastUpdated: '09 Feb 2026'
+  chromeExtensionLastUpdated: '26 Feb 2026'
 };
